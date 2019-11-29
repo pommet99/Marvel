a8c9b88707ac1a0e2be091c47c75fbdd1825e354
Prameet <47789905+Prameet@users.noreply.github.com>
2019-11-29 14:40:07 +0100

    added the code
    did the swapi code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,78 @@
-function getApiData() {
-    const xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            const data = JSON.parse(xhr.responseText);
+var go;
+var clear;
 
-        }
-    }     
-    xhr.open('GET', 'https://developer.marvel.com/docs#!/public/getCharacterIndividual_get_1');
-    xhr.send()
+
+var textName;
+var textBirth;
+var param;
+
+
+$(function() {
+  go = $("#go");
+  clear = $('#clear');
+	img = $('#img');
+  textName = $('#text_name');
+  textBirth = $('#text_birth');
+  param = $('#input');
+});
+
+function clearData() {
+	img.attr('src', '');
+  textName.html('');
+  textBirth.html(''); 
+  param.val('');
 }
+
+function submit(){
+  var inputValue = $.trim(param.val());
+  if (inputValue === '') return;
+  
+  var swURL = "https://swapi.co/api/people/" + param.val();
+
+  $.ajax({
+    type: "GET",
+    url: swURL,
+    success: function(data){ 
+      ajaxSuccess(data);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+       ajaxError();
+    }
+  });
+}
+
+function random() {
+   var number = Math.floor(Math.random() * 88) + 1;
+	 var swURL = "https://swapi.co/api/people/" + number;
+
+  $.ajax({
+    type: "GET",
+    url: swURL,
+    success: function(data){ 
+      ajaxSuccess(data);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+       ajaxError();
+    }
+  });
+}
+
+function ajaxSuccess(data){
+  var name = data.name;
+  var birth = data.birth_year;
+	var imageURI;
+	
+	
+	img.attr('src', imageURI);
+  textName.html(name);
+  textBirth.html(birth); 
+	
+  clear.click(function() {
+    clearData();
+  });
+}
+
+function ajaxError(data){
+  clearData();
+}
+
